0b39835138e2c0ed723b7af7c760d3f4fccc6241
miranda-sanchez <sanchezmiranda078@gmail.com>
2023-08-14 19:46:01 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@ enterButton.addEventListener("click", function () {
   section.style.display = "none";
 
   images.forEach((img, index) => {
-    img.style.transform = "scale(5)";
+    img.style.width = "300%";
     img.style.position = "fixed";
+    img.style.transition = "width 3s ease-in";
 
-    const transitionDuration = index === 0 ? "5s" : index === 1 ? "5s" : "3s";
-    img.style.transition = `transform ${transitionDuration} linear`;
+    /*const transitionDuration = index === 0 ? "6s" : index === 1 ? "3s" : "2s";
+    img.style.transition = `height ${transitionDuration} linear`;*/
   });
 
   setTimeout(function () {
     const firstView = document.querySelector(".view:first-child");
     firstView.style.display = "none";
-  }, 6000);
+    const secondView = document.querySelector(".view:nth-child(2)");
+    secondView.style.display = "block";
+  }, 3000);
 });
